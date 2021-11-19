79bceb6bf60636282eb546173e861d357a8ec5ae
chick_0 <chick_0@ch1ck.xyz>
2021-11-20 04:37:57 +0900

    init

---
+++ src/images.js
@@ -0,0 +1,28 @@
+export default {
+    a: [
+        "/assets/a/1.jpg",
+        "/assets/a/2.jpg",
+    ],
+    b: [
+        "/assets/b/1.jpg",
+        "/assets/b/2.jpg",
+        "/assets/b/3.jpg",
+        "/assets/b/4.jpg"
+    ],
+    c: [
+        "/assets/c/1.jpg",
+        "/assets/c/2.jpg",
+        "/assets/c/3.jpg",
+        "/assets/c/4.jpg"
+    ],
+    d: [
+        "/assets/d/1.jpg",
+        "/assets/d/2.jpg",
+        "/assets/d/3.jpg",
+        "/assets/d/4.jpg"
+    ],
+    e: [
+        "/assets/e/1.jpg",
+        "/assets/e/2.jpg",
+    ]
+}
